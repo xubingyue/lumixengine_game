fc5c755bdcb4b463288757310385db7f0eef216e
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-11-22 01:43:21 +0100

    nature pack by kenney

---
+++ lua/import.lua
@@ -0,0 +1,29 @@
+function importDir(src_dir)
+	for filename in io.popen([[dir "]] .. src_dir .. [[" /b]]):lines() do 
+		if string.sub(filename,-string.len(".FBX")):upper() ==".FBX" and string.find(filename, "_LOD") == nil then
+			importModel(src_dir .. filename, [[models/environment/]])
+		end
+	end
+end
+
+function importModel(filename, out_dir)
+	Engine.logInfo("Importing model " .. filename .. "...")
+	ImportAsset.clearSources()
+	ImportAsset.addSource(filename)
+	ImportAsset.setParams({
+		output_dir = out_dir,
+		scale = 1.0,
+	})
+	for i = 0, ImportAsset.getMaterialsCount() - 1 do
+		local args = {import = true, alpha_cutout = false}
+		local name = ImportAsset.getMaterialName(i)
+		ImportAsset.setMaterialParams(i, args)
+		
+	end
+
+	
+	ImportAsset.import()
+	Engine.logInfo(filename .. " imported")
+end
+
+importDir([[source_assets/naturepack/]])
