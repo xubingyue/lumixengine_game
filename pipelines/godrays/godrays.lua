4f3c0031212ce6a7c654a88a393612c632cf0ecd
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-01-01 14:10:14 +0100

    fixed godrays

--- pipelines/godrays/godrays.lua
+++ pipelines/godrays/godrays.lua
@@ -5,8 +5,8 @@ decay = 0.99
 weight = 0.1
 
 function initPostprocess(pipeline, env)
-	env.ctx.godrays_material = Engine.loadResource(g_engine, "pipelines/godrays/godrays.mat", "material")
-	env.ctx.godrays_params = createVec4ArrayUniform(pipeline, "u_godrays_params", 1)
+	env.godrays_material = Engine.loadResource(g_engine, "pipelines/godrays/godrays.mat", "material")
+	env.godrays_params = createVec4ArrayUniform(pipeline, "u_godrays_params", 1)
 end
 
 
@@ -15,10 +15,10 @@ function postprocess(pipeline, env)
 	newView(pipeline, "godrays", "hdr")
 		local godrays_params = {exposure, decay, weight, 0}
 		setActiveGlobalLightUniforms(pipeline)
-		setUniform(pipeline, env.ctx.godrays_params, {godrays_params})
+		setUniform(pipeline, env.godrays_params, {godrays_params})
 		setPass(pipeline, "MAIN")
 		enableBlending(pipeline, "add")
 		disableDepthWrite(pipeline)
-		bindFramebufferTexture(pipeline, "g_buffer", 3, env.ctx.texture_uniform)
-		drawQuad(pipeline, 0, 0, 1, 1, env.ctx.godrays_material)
+		bindFramebufferTexture(pipeline, "g_buffer", 3, env.texture_uniform)
+		drawQuad(pipeline, 0, 0, 1, 1, env.godrays_material)
 end
