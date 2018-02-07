65a6af7d687b914b8d2a8d7460d55d0f1440140e
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-02-07 16:27:48 +0100

    particles

--- cs/Car.cs
+++ cs/Car.cs
@@ -8,11 +8,15 @@ public class Car : Component
 	public Entity TimeUI;
 	public Entity SpeedUI;
 	public Entity Mesh;
+	public Entity LeftWheelParticles;
+	public Entity RightWheelParticles;
 	public float Force = 5000;
 
 	SphereRigidActor _Collider;
 	GuiText _TimeText;
 	GuiText _SpeedText;
+	ParticleEmitter _LeftParticlesEmitter;
+	ParticleEmitter _RightParticlesEmitter;
 	Dictionary<uint, bool> _Keys = new Dictionary<uint, bool>();
 	float _Yaw;
 	float _Time = 0;
@@ -24,6 +28,8 @@ public class Car : Component
 		_Collider = ColliderEntity.GetComponent<SphereRigidActor>();
 		_TimeText = TimeUI.GetComponent<GuiText>();
 		_SpeedText = SpeedUI.GetComponent<GuiText>();
+		_LeftParticlesEmitter = LeftWheelParticles.GetComponent<ParticleEmitter>();
+		_RightParticlesEmitter = RightWheelParticles.GetComponent<ParticleEmitter>();
 	}
 
 	private float getRoll(Vec3 dir, Vec3 vel)
@@ -56,9 +62,13 @@ public class Car : Component
 			_Collider.ApplyForceToActor(dir * velocity.Length * 4);
 		}
 
-		var roll = new Quat(new Vec3(0, 0, 1), getRoll(dir, velocity));
-		entity.Rotation = new Quat(new Vec3(0, 1, 0), _Yaw);
-		Mesh.SetLocalRotation(roll);
+		float roll_angle = getRoll(dir, velocity);
+		var roll = new Quat(new Vec3(0, 0, 1), roll_angle);
+		Mesh.Rotation = new Quat(new Vec3(0, 1, 0), _Yaw) * roll;
+		Mesh.Position = entity.Position;
+		float camera_yaw = _Yaw;
+		if(velocity.Length > 0.01f) camera_yaw = Mathf.Atan2(velocity.x, velocity.z);
+		entity.Rotation = new Quat(new Vec3(0, 1, 0), camera_yaw);
 
 		if (IsKeyDown('d')) _Yaw -= time_delta;
 		if (IsKeyDown('a')) _Yaw += time_delta;
@@ -68,6 +78,18 @@ public class Car : Component
 			force.x = Mathf.Sin(_Yaw) * time_delta * Force;
 			force.z = Mathf.Cos(_Yaw) * time_delta * Force;
 			_Collider.ApplyForceToActor(force);
+		}
+
+		// particles
+		if(velocity.Length < 3)
+		{
+			_LeftParticlesEmitter.IsAutoemit = false;
+			_RightParticlesEmitter.IsAutoemit = false;
+		}
+		else
+		{
+			_LeftParticlesEmitter.IsAutoemit = roll_angle < -0.1f;
+			_RightParticlesEmitter.IsAutoemit = roll_angle > 0.1f;
 		}
 	}
 
